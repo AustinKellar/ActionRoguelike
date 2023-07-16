5de6c4ca191d76a176eda1cd1ac195c41497fb5f
AustinKellar <akellar@ucsc.edu>
2023-07-15 18:36:07 -0700

    Improve character movement and camera control

--- Source/ActionRoguelike/Character/SCharacter.h
+++ Source/ActionRoguelike/Character/SCharacter.h
@@ -32,11 +32,12 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
+	
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 private:
 	void MoveForward(float Value);
+	void MoveRight(float Value);
 
 };
